e9342d96d3dd3172a181f72ab56131d40524b681
ConorFoley <conorfoooley@gmail.com>
2021-09-15 14:17:08 -0300

    fix: using price/usdt

--- app/modules/routes/marketDetails/index.ts
+++
@@ -1,5 +0,0 @@
-import { loadMarketDetails } from './get';
-
-export const marketDetails = async (router: any, options: any) => {
-  router.get('/:exchangeName/:symbol', loadMarketDetails);
-}
--- app/modules/routes/routes.ts
+++ app/modules/routes/routes.ts
@@ -1,7 +1,6 @@
 import { user } from "./user";
 import { log } from "./logger";
 import { marketOverview } from './marketOverview';
-import { marketDetails } from './marketDetails';
 
 /**
  * Creates the array of routes to be set up.
@@ -15,6 +14,5 @@ export function routes(app: any): Array<Promise<any>> {
         app.register(log, { prefix: 'ws/v2/log' }),
         app.register(user, { prefix: 'ws/v2/user' }),
         app.register(marketOverview, { prefix: 'ws/v2/mktOverview' }),
-        app.register(marketDetails, { prefix: 'ws/v2/marketDetails' }),
     ];
 }
