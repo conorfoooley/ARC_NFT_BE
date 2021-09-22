439c92d081b8d81ed553913cb25b81fdb9120674
ConorFoley <conorfoooley@gmail.com>
2021-09-22 15:07:26 -0300

    create market router (#10)

--- app/modules/interfaces/IAPIKey.ts
+++ app/modules/interfaces/IAPIKey.ts
@@ -1,11 +1,5 @@
-export interface IExtraApiKeyFields {
-    fieldName: string,
-    value: any
-}
-
 export interface IAPIKey {
     id: string
     apiKey: string
     apiSecret?: string
-    extraFields?: IExtraApiKeyFields[]
 }
