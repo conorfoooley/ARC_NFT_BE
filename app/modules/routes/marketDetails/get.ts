8f7e35fea0643b52fb3845ca7476a88e0ea2d2fc
ConorFoley <conorfoooley@gmail.com>
2021-12-14 11:01:21 -0300

    feat: place order future and fix order list

--- app/modules/routes/marketDetails/get.ts
+++ app/modules/routes/marketDetails/get.ts
@@ -2,6 +2,10 @@ import * as ccxt from 'ccxt';
 import { FastifyReply, FastifyRequest } from "fastify";
 import { respond } from "../../util/respond";
 
+const dotenv = require("dotenv");
+dotenv.config();
+// process.env["MONGODB_USER"],
+
 export const loadMarketDetails = async (req: FastifyRequest, res: FastifyReply) => {
 
   const { exchangeName, symbol } = req.params as any;
@@ -29,15 +33,16 @@ export const loadMarketDetails = async (req: FastifyRequest, res: FastifyReply) 
 export const loadAllExchangesOrderBook = async(req: FastifyRequest, res: FastifyReply) => {
   
   let allExchanges = ['binance', 'huobi', 'ftx', 'kucoin'];
-  const { symbol } = req.params as any;
+  const { marketType, symbol } = req.params as any;
 
-  const formattedSymbol = symbol.replace('-', '/');
+
   let allExchangesOrderBook = [];
   
   if (symbol) {
     try {
       for (const exchangeName of allExchanges) {
         const exchange = new ccxt[exchangeName]();
+        exchange.options.defaultType = marketType
 
         if(exchangeName === 'kucoin'){
           exchange.apiKey = process.env["KUCOIN_SERVICE_API_KEY"];
@@ -47,10 +52,13 @@ export const loadAllExchangesOrderBook = async(req: FastifyRequest, res: Fastify
         }
 
         const markets = await exchange.loadMarkets();
-        if (markets[formattedSymbol]) {
-          const response = await exchange.fetchOrderBook(formattedSymbol);
+        const formattedSymbol = symbol.replace('-', '/');
+        const realSymbol = markets[symbol] ? symbol : markets[formattedSymbol] ? formattedSymbol : undefined
+
+        if (realSymbol) {
+          const response = await exchange.fetchOrderBook(realSymbol);
           const precision = {amount: 4 , base: 8 , price: 6 , quote: 8};
-          allExchangesOrderBook.push({exchangeName: exchange.name, orderBook: response ? response : {}, precision});
+          allExchangesOrderBook.push({exchangeName: exchange.name, orderBook: response, precision});
         }
       }
     } catch (error) {
@@ -59,6 +67,7 @@ export const loadAllExchangesOrderBook = async(req: FastifyRequest, res: Fastify
 
     if(allExchangesOrderBook.find(order => order.exchangeName === 'KuCoin')){
       allExchangesOrderBook.find(order => order.exchangeName === 'KuCoin').orderBook.asks = (allExchangesOrderBook.find(order => order.exchangeName === 'KuCoin').orderBook.asks).slice(0,10);
+      allExchangesOrderBook.find(order => order.exchangeName === 'KuCoin').orderBook.bids = (allExchangesOrderBook.find(order => order.exchangeName === 'KuCoin').orderBook.bids).slice(0,10);
     }
     
     return res.send({ 
