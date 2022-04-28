becdd6f4b4c809688654f1aa3ec2ee794bcdcf7e
ConorFoley <conorfoooley@gmail.com>
2022-04-28 23:00:13 +0700

    create rewards

---
+++ app/modules/interfaces/INFTReward.ts
@@ -0,0 +1,12 @@
+
+export interface INFTReward {
+    _id?: string;    
+    wallet: string;  
+    score?: number;  
+    reward?:number;
+    scoreCollection?:number;
+    pnft?:number;
+    listingScore?:number;
+    listingReward?:number; 
+  }
+  
